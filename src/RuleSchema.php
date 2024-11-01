599559f4e96d4d37d77aecef08b50c0fc8d9e53d
alpayklncrsln <alpay66fb@hotmail.com>
2024-11-01 20:24:48 +0300

    database schema added static function

--- src/RuleSchema.php
+++ src/RuleSchema.php
@@ -5,6 +5,9 @@ namespace Alpayklncrsln\RuleSchema;
 use Alpayklncrsln\RuleSchema\Interfaces\RuleSchemaInterface;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class RuleSchema implements RuleSchemaInterface
 {
@@ -73,5 +76,66 @@ class RuleSchema implements RuleSchemaInterface
     {
         if (!Auth::check()) $this->merge(...$rules);
         return $this;
+    }
+
+    public static function model(string $table) : RuleSchema
+    {
+        $ruleSchema = RuleSchema::create();
+        $columns = collect(Schema::getColumns($table))
+            ->filter(function($column) {
+                return !in_array($column['name'], ['id', 'created_at', 'updated_at']) ? $column : null;
+            });
+        $rule = null;
+        foreach ($columns as $column) {
+            $rule = Rule::make($column['name']);
+            if (Str::StartsWith($column['type'], 'varchar')) {
+                $rule->string()->max((int) Str::between($column['type'], '(', ')'));
+            }
+            if ($column['type'] === 'int') {
+                $rule->numeric();
+            }
+
+            if ($column['type'] === 'text') {
+                $rule->string();
+            }
+
+            if ($column['type'] === 'tinyint') {
+                $rule->boolean();
+            }
+
+            if ($column['type'] === 'date') {
+                $rule->date();
+            }
+
+            if ($column['type'] === 'datetime') {
+                $rule->dateTime();
+            }
+
+            if ($column['type'] === 'timestamp') {
+                $rule->dateTime();
+            }
+
+            if ($column['type'] === 'json') {
+                $rule->json();
+            }
+
+            if ($column['type'] === 'jsonb') {
+                $rule->json();
+            }
+
+            if ($column['type'] === 'float') {
+                $rule->numeric();
+            }
+
+            if ($column['type'] === 'decimal') {
+                $rule->decimal(8);
+            }
+
+            if ($column['type'] === 'bigint') {
+                $rule->numeric();
+            }
+            $ruleSchema->merge($rule);
+        }
+            return $ruleSchema;
     }
 }
