00fd742eb9feb7ae79354d7e76bc86d8a32fb71b
alpayklncrsln <alpay66fb@hotmail.com>
2024-11-01 20:23:06 +0300

    DefaultRuleSchema added

---
+++ src/Default/DefaultRuleSchema.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Alpayklncrsln\RuleSchema\Default;
+
+use Alpayklncrsln\RuleSchema\Rule;
+use Alpayklncrsln\RuleSchema\RuleSchema;
+
+class DefaultRuleSchema
+{
+    public static function login():RuleSchema
+    {
+        return RuleSchema::create(
+            Rule::make('email')->required()->email()->max()->exists('users','email'),
+            Rule::make('password')->required()->min(8)->max()
+        );
+    }
+
+    public static function register():RuleSchema
+    {
+        return RuleSchema::create(
+            Rule::make('name')->required()->max(),
+            Rule::make('email')->required()->email()->max()->unique('users','email'),
+            Rule::make('password')->required()->min(8)->max()->confirmed(),
+        );
+    }
+
+    public static function resetPassword():RuleSchema
+    {
+        return RuleSchema::create(
+            Rule::make('email')->required()->email()->max()->exists('users','email'),
+            Rule::make('token')->required()->exists('password_resets','token'),
+            Rule::make('password')->required()->min(8)->max()->confirmed(),
+        );
+    }
+
+    public static function updatePassword():RuleSchema
+    {
+        return RuleSchema::create(
+            Rule::make('password')->required()->min(8)->max()->confirmed(),
+        );
+    }
+
+}
