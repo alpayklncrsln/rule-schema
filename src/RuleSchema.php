ad135ada9e07c5ffb0850fdec3ed411408b85d55
alpayklncrsln <alpayklncrsln@users.noreply.github.com>
2024-11-04 10:15:06 +0000

    Fix styling

--- src/RuleSchema.php
+++ src/RuleSchema.php
@@ -17,10 +17,12 @@ class RuleSchema implements RuleSchemaInterface
 
     public function __construct(Rule ...$rules)
     {
-        if (count($rules) > 0) $this->merge(...$rules);
+        if (count($rules) > 0) {
+            $this->merge(...$rules);
+        }
     }
 
-    public static function create(Rule...$rules): self
+    public static function create(Rule ...$rules): self
     {
         return new RuleSchema(...$rules);
     }
@@ -34,62 +36,74 @@ class RuleSchema implements RuleSchemaInterface
                 $this->setCacheData();
             }
         }
+
         return $this->rules;
     }
 
     public function merge(Rule ...$rules): self
     {
-        if (!$this->existsCacheData()){
+        if (! $this->existsCacheData()) {
             foreach ($rules as $rule) {
                 $this->rules = array_merge($this->rules, $rule->getRule());
             }
         }
+
         return $this;
     }
 
     public function ruleClass(string $attribute, ValidationRule $class): self
     {
-        if (!$this->existsCacheData()){
+        if (! $this->existsCacheData()) {
             $this->rules[$attribute][] = $class;
         }
+
         return $this;
     }
 
     public function when(bool $condition, Rule ...$rules): self
     {
-        if ($condition && !$this->existsCacheData()) {
+        if ($condition && ! $this->existsCacheData()) {
             $this->merge(...$rules);
         }
+
         return $this;
     }
 
     public function expect(string ...$attributes): self
     {
-        if (!$this->existsCacheData()) {
+        if (! $this->existsCacheData()) {
             foreach ($attributes as $attribute) {
                 unset($this->rules[$attribute]);
             }
         }
+
         return $this;
     }
 
     public function existsMerge($attribute, Rule ...$rules): self
     {
-        if (isset($this->rules[$attribute]) && !$this->existsCacheData()) {
+        if (isset($this->rules[$attribute]) && ! $this->existsCacheData()) {
             $this->merge(...$rules);
         }
+
         return $this;
     }
 
     public function auth(Rule ...$rules): self
     {
-        if (Auth::check() && !$this->existsCacheData()) $this->merge(...$rules);
+        if (Auth::check() && ! $this->existsCacheData()) {
+            $this->merge(...$rules);
+        }
+
         return $this;
     }
 
     public function notAuth(Rule ...$rules): self
     {
-        if (!Auth::check() && !$this->existsCacheData()) $this->merge(...$rules);
+        if (! Auth::check() && ! $this->existsCacheData()) {
+            $this->merge(...$rules);
+        }
+
         return $this;
     }
 
