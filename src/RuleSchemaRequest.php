749e66ca1333fc70e58d8a5e0e9d524c16a8c442
alpayklncrsln <alpay66fb@hotmail.com>
2025-02-10 01:18:04 +0300

    RuleSchema form request class added

---
+++ src/RuleSchemaRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Alpayklncrsln\RuleSchema;
+
+use Alpayklncrsln\RuleSchema\Interfaces\HasRuleSchema;
+use Illuminate\Foundation\Http\FormRequest;
+
+class RuleSchemaRequest extends FormRequest implements HasRuleSchema
+{
+
+    public function rules(): array
+    {
+        return $this->ruleSchema()->getRules();
+    }
+
+    public function messages(): array
+    {
+        return $this->ruleSchema()->getMessages();
+    }
+
+    public function ruleSchema(): RuleSchema
+    {
+        return RuleSchema::create();
+    }
+}
